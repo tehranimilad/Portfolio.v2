78d1c428195b8d33a6a2a5223b3a4b9117d57edf
Milad Tehrani <tehranimilad92@gmail.com>
2023-01-26 12:58:05 -0800

    building out landing page and animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './pages/Home';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
       <div className="App">
-        <HomePage />
+        {/* <HomePage /> */}
+        <LandingPage />
       </div>
     </>
   );
