5cac04a46141c6165a30e7c3a0e9c3cdc081a7ba
Milad Tehrani <tehranimilad92@gmail.com>
2023-01-26 12:11:43 -0800

    building out frontend file structure

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+const HomePage = () => {
+    useEffect(() => {
+
+    
+}, []);
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+export default HomePage;
