87ca8de990f524dd378acbbcef80cdb6c988e2dc
Milad Tehrani <tehranimilad92@gmail.com>
2023-02-02 14:38:48 -0800

    deployment

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const port  = process.env.PORT
 // Middleware
 // set folder for static files
 app.use(express.static('public'))
-// sets the view engine to EJS for our app (this allows us to render EJS files without usind `.ejs` after file names)
-app.set('view engine', 'ejs')
 app.use(methodOverride('_method'));  
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
