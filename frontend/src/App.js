2ad52add5e28e61176ca1c52d29902a8a331ee14
Milad Tehrani <tehranimilad92@gmail.com>
2023-02-03 10:29:41 -0800

    deploying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,23 @@ import HomePage from './pages/HomePage';
 import ProjectPage from './pages/Projects';
 import LearnMore from './pages/LearnMore';
 import Nav from './components/nav';
+const express = require('express')
+const app = express();
+
+const path = require('path')
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req,res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
+
+app. listen(port, (err) => {
+  if(err) return console.log(err);
+  console.log("server listening on port: ", port);
+})
 
 import { Routes, Route } from "react-router-dom";
 
