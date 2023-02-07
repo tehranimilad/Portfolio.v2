c2ab53807e096765ec3c78bb8a00ae4b27de54eb
Milad Tehrani <tehranimilad92@gmail.com>
2023-02-06 18:34:43 -0800

    cleaning up nav page

--- frontend/src/components/nav/index.js
+++ frontend/src/components/nav/index.js
@@ -6,18 +6,17 @@ function Nav() {
   
     const navigate = useNavigate()
     
-    // Allow user to log out, clears local storage, sets login status to false, and then navigate back home.  
   
       return (
         <Navbar className="justify-content-end">
           <div>
             <a className="navIcons" href="https://www.linkedin.com/in/miladtehrani/" target="_blank">
-                <img
-                src="https://seeklogo.com/images/L/linkedin-icon-logo-32AA14A009-seeklogo.com.png"
-                alt="Linked in Logo"
-                width="50"
-                className="navImages"
-                />
+            <img
+            src="https://seeklogo.com/images/L/linkedin-icon-logo-32AA14A009-seeklogo.com.png"
+            alt="Linked in Logo"
+            width="50"
+            className="navImages"
+            />
             </a>
             </div>
             <div>
@@ -31,11 +30,11 @@ function Nav() {
             </div>
             <div>
             <a className="navIcons" href="https://docs.google.com/document/d/1MzWaSuGeCjDPhTjd3XrxExC7DQkJxuk7/edit?usp=sharing&ouid=117772898957068889268&rtpof=true&sd=true" target="_blank">
-                <img
-                src="http://cdn.onlinewebfonts.com/svg/img_235431.png"
-                alt="Resume Logo"
-                className="navImages"
-                />
+            <img
+            src="http://cdn.onlinewebfonts.com/svg/img_235431.png"
+            alt="Resume Logo"
+            className="navImages"
+            />
             </a>
             </div>
         </Navbar>
