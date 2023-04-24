cca690f1b07e770b273ff8ec0e5432c5cd88ef26
Milad Tehrani <tehranimilad92@gmail.com>
2023-04-24 15:57:35 -0700

    updating src imports

--- frontend/src/pages/Projects/index.js
+++ frontend/src/pages/Projects/index.js
@@ -1,8 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';import Card from 'react-bootstrap/Card';
 import Nav from '../../components/nav';
 import cruiseAmerica from './assets/screenRecording1.gif'
 import cruiseAmerica2 from './assets/screenRecording2.gif'
