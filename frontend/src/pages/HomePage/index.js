1925381ef53889c55fc6573d46602581fb284f06
Milad Tehrani <tehranimilad92@gmail.com>
2023-04-26 15:43:38 -0700

    added  html and css logos to homepage

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -35,14 +35,27 @@ const HomePage = () => {
             <h1 className="noBold">Hey I'm <span className="nameBold">Milad Tehrani</span></h1>
             <h1 className="noBold">I'm A <span className="nameBold">Software Engineer</span></h1>
             </div> 
+            
             <div className="skillSet">
             <a className="skillLink">
               <span className="skillText">JavaScript</span>
               <img className="skillImg" src="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png" alt="JS" />
             </a>
             <a className="skillLink">
+              <span className="skillText">HTML</span>
+              <img className="skillImg2" src="https://cdn.worldvectorlogo.com/logos/html-1.svg" alt="HTML" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">CSS</span>
+              <img className="skillImg3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS" />
+            </a>
+            <a className="skillLink">
               <span className="skillText">React</span>
               <img className="skillImg2" src=" https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="React" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">Python</span>
+              <img className="skillImg2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" alt="Python" />
             </a>
             <a>Javascript, React, Python, MongoDB, Node.js, Express, SQL, HTML, CSS, Bootstrap</a>
             </div>
