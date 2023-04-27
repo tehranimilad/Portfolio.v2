6def2db95e2ee2a9f6414b27059960f965020fcd
Milad Tehrani <tehranimilad92@gmail.com>
2023-04-27 15:51:20 -0700

    adding content and optimizing placement on single page

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -36,36 +36,30 @@ const HomePage = () => {
       return (
           <>
             <Nav />
+            {/* <div>
+            <h1 className="noBold">Hey I'm <span className="nameBold">Milad Tehrani</span></h1>
+            <h1 className="noBold">I'm A <span className="nameBold">Software Engineer</span></h1>
+            </div> */}
             <div>
+           
+                <Container className="learnMoreInfo">
+                  
+                    <Card className="text-center">
+                    <div>
             <h1 className="noBold">Hey I'm <span className="nameBold">Milad Tehrani</span></h1>
             <h1 className="noBold">I'm A <span className="nameBold">Software Engineer</span></h1>
             </div>
-            <div>
-                <Image className="avatar" src={headshot} fluid></Image>
-           
-                <Container className="learnMoreInfo">
-                    <Card className="text-center">
+                    <Image className="avatar" src={headshot} fluid></Image>
                     <Card.Body>
-                        <Card.Title className="projectTitle">
+                        {/* <Card.Title className="projectTitle">
                             Milad Tehrani
-                        </Card.Title>
+                        </Card.Title> */}
                         <Card.Text className="projectDesc">
                         I am a highly skilled software engineer with a background in Management Information Systems, dedicated to delivering innovative solutions that improve business processes and drive results. With a passion for technology, I leverage my expertise to solve complex problems and help businesses achieve their goals. 
                         </Card.Text>
                         <Card.Title className="projectTitle">Skills</Card.Title>
                         <Card.Text className="projectDesc">
-                        Javascript, React, Python, MongoDB, Node.js, Express, SQL, HTML, CSS, Bootstrap
-                        </Card.Text>
-                
-        
-                    </Card.Body>
-                    </Card>
-                </Container>
-        </div>
-
-
-
-            <div className="skillSet">
+                        <div className="skillSet">
             <a className="skillLink">
               <span className="skillText">JavaScript</span>
               <img className="skillImg" src="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png" alt="JS" />
@@ -110,6 +104,61 @@ const HomePage = () => {
 
         
             </div>
+                        </Card.Text>
+                
+        
+                    </Card.Body>
+                    </Card>
+                </Container>
+        </div>
+
+
+
+            {/* <div className="skillSet">
+            <a className="skillLink">
+              <span className="skillText">JavaScript</span>
+              <img className="skillImg" src="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png" alt="JS" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">HTML</span>
+              <img className="skillImg2" src="https://cdn.worldvectorlogo.com/logos/html-1.svg" alt="HTML" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">CSS</span>
+              <img className="skillImg3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">React</span>
+              <img className="skillImg2" src=" https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="React" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">Python</span>
+              <img className="skillImg2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" alt="Python" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">MongoDB</span>
+              <img className="skillImg2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Antu_mongodb.svg/2048px-Antu_mongodb.svg.png" alt="MongoDb" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">Node.js</span>
+              <img className="skillImg4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png" alt="Nodejs" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">Express.js</span>
+              <img className="skillImg4" src="https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png" alt="Express" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">SQL</span>
+              <img className="skillImg3" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" alt="SQL" />
+            </a>
+            <a className="skillLink">
+              <span className="skillText">Bootstrap</span>
+              <img className="skillImg4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png" alt="Bootstrap" />
+            </a>
+          
+
+        
+            </div> */}
               {/* <div className="landing-page-container">
               <div className="crypto-animation" fluid>
               <div className="projects">
